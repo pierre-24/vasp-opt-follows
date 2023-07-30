3393a0c20c682e91a6f4f30ed334e0f0564252a9
Pierre Beaujean <pierreb24@gmail.com>
2023-07-30 16:49:52 +0200

    Bump version: 0.1.0 → 0.2.0

--- vasp_opt_follows/__init__.py
+++ vasp_opt_follows/__init__.py
@@ -2,7 +2,7 @@
 Follows different criterion of an optimization done with VASP.
 """
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Pierre Beaujean'
 __maintainer__ = 'Pierre Beaujean'
 __email__ = 'pierre.beaujean@unamur.be'
